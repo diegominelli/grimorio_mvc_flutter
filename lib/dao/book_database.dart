ba23145d2bc41d72b1ee3a520b44661336e20998
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2025-09-06 11:14:51 -0300

    organizando os models

--- lib/dao/book_database.dart
+++ lib/dao/book_database.dart
@@ -4,7 +4,7 @@ import 'package:sqflite/sqflite.dart';
 // ignore: depend_on_referenced_packages
 import 'package:path/path.dart';
 
-import '../servives/google_book_service.dart';
+import '../models/personal_book.dart';
 
 class PersonalBookDatabase {
   static const String _tableName = "bookTable";
@@ -104,24 +104,3 @@ class PersonalBookDatabase {
 }
 
 class PersonalBookNotFindException implements Exception {}
-
-// These below are just examples. Need to create new models
-class PersonalBook {
-  int id = 0;
-  GoogleBook googleBook = GoogleBook(
-    authors: "a",
-    description: "b",
-    id: "c",
-    thumbnailLink: "d",
-    title: "e",
-  );
-
-  PersonalBook.fromMap(Map<String, dynamic> map) {
-    id = map["id"];
-    googleBook = map["googleBook"];
-  }
-
-  Map<String, dynamic> toMap() {
-    return {"id": id, "googleBook": googleBook};
-  }
-}
